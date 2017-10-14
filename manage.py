49608a4ccabf4f7cf9b87c8c9ba06fa47b42eaea
pollman <caleb.pollman@zeeram.com>
2017-10-14 17:20:07 -0600

    completed lesson 1, section 6

--- manage.py
+++ manage.py
@@ -2,18 +2,13 @@ import unittest
 
 from flask_script import Manager
 
-from project import app, db
+from project import create_app, db
+from project.api.models import User
 
 
+app = create_app()
 manager = Manager(app)
 
-
-@manager.command
-def recreate_db():
-    """Recreates a database"""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 @manager.command
 def test():
@@ -23,6 +18,14 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database"""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
